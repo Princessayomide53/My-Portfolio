d7db3aa97cc42610b2ab4cc58db54d4a31773b90
Princess Ayomide Ogunnaike <Payomide53@gmail.com>
2025-07-10 22:58:30 +0100

    Added new pages

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from './Nav';
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen z-10'>
+      <div className='absolute inset-0'>
+        <Nav />
+      </div>
+
+      <video autoPlay loop playsInline muted className='w-full'>
+        <source src='/Assets/nextbot.mp4' type='video/mp4' />
+      </video>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <header className='flex justify-between max-w-7xl mx-auto'>
-      <h1>Logo</h1>
+    <header className='flex text-white justify-between max-w-7xl mx-auto relative z-20 py-10'>
+      <h1 className='text-2xl'>Logo</h1>
 
       <nav>
-        <ul className='flex space-x-8'>
+        <ul className='flex space-x-8 text-xl'>
           <li>Home</li>
           <li>About</li>
           <li>Works</li>
